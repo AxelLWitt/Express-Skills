18436786ae7cd6c9f823ad89d37923a759341ff0
AxelLWitt <axellwitt16@gmail.com>
2023-08-22 19:35:36 -0400

    finally done. love apple /s

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express-ing my frustration of using sub-optimal hardware to learn stuff', 
+  message:'the list of skills I am accumulating using a horrible laptop!' });
 });
 
 module.exports = router;
